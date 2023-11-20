9558eecccda70f3e606628b8e58f530b3d4c4687
Oleksiy Vekhulevskyy <alexvekh@yahoo.com>
2023-11-19 23:29:50 -0500

    added useDispatch

--- src/container/counter/index.tsx
+++ src/container/counter/index.tsx
@@ -1,17 +1,20 @@
-import { useSelector } from "react-redux";
+import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "../../store";
+import { increment, decrement, incrementByAmount } from "./slice";
 
 export function Counter() {
   const count = useSelector((state: RootState) => state.counter.value);
+  const dispatch = useDispatch();
 
   return (
     <div className="counter">
-      <button className="button" onClick={() => null}>
+      <button className="button" onClick={() => dispatch(decrement())}>
         {" "}
         -{" "}
       </button>
       <span className="value"> {count} </span>
-      <button className="button" onClick={() => null}>
+      <button className="button" onClick={() => dispatch(increment())}>
+        {/* dispatch(incrementByAmount({ value: 10 })) */}
         {" "}
         +{" "}
       </button>
